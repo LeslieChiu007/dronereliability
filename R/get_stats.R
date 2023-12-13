8e030de8a53481c5ac784b65697a1e9dffe59954
LeslieChiu007 <hz298@cornell.edu>
2023-12-13 22:02:52 +0000

    ....

---
+++ R/get_stats.R
@@ -0,0 +1,282 @@
+#' @name get_stats
+#' @title GET STATS AND PLOT
+#' @author Hengtao Zhao & Andres Garcia
+#' @param mot1_MTTF ....
+#' @param mot2_MTTF ....
+#' @param prop1_MTTF ....
+#' @description
+#' 
+#' This function will calculate the reliability of a commercial drone system.
+#' It splits a commercial drone into six different systems: Ground Control,
+#' Mainframe, Power System, Payload, Electronics System, and Navigation Systems.
+#' We further broke down the Power System into 3 main components, the battery,
+#' motors, and propellers. The failure rates for the 6 systems were derived from
+#' literary research and the mean time to failure of the batteries, motors, and
+#' propellers were found by using the most common commercial options.
+#'
+#' The MTTF for the original motor and propellers (mot1 and mot2) are the lower ends
+#' of the ranges that we found online for drone motors and propellers. The MTTF for
+#' the improved motors and propellers, (mot2 and prop2) are the higher ends of the 
+#' ranges that we found online. Using these values, we are comparing the reliability
+#' as a result of using higher quality components. In this function you are free to
+#' enter your own MTTF for intiial and final motor/battery components and observe
+#' how this affects a drone's reliability.
+#'
+#' For the batteries, our initial battery component (bat1) is a lithium ion battery
+#' system. We found its MTTF by multiplying the average lifetime of one cycle (bat1_t)
+#' by the
+#' expected life cycles. 
+#' @export
+
+get_stats = function(mot1_MTTF = 8000, mot2_MTTF = 12000, prop1_MTTF = 300, 
+                             prop2_MTTF=500, bat1_t = 800, bat1_lc = 400,
+                             bat2_t=1090, bat2_lc=1000,
+                             lambda_gc = 2.00*10^-6, lambda_main = 2.77*10^-6,
+                             lambda_nav = 9.41*10^-6, lambda_es = 5.01*10^-6,
+                             lambda_pay = 1.10*10^-6){
+  
+  # Load packages
+  library(dplyr)
+  library(ggplot2)
+  library(tibble)
+  library(ggplot2)
+  library(dplyr)
+  library(magrittr)
+  library(tidyverse)
+  library(mosaicCalc)
+  library(ggpubr)
+  library(moments)
+  library(broom)
+  
+  
+  # Failure rate for the a drone's ground control, this value was found
+  # through literary research
+  #lambda_gc = 2.00 * 10^-6
+  
+  # Failure rate for the a drone's mainframe, this value was found
+  # through literary research
+  #lambda_main = 2.77 * 10^-6
+  
+  # Failure rate for the a drone's navigation system, this value was found
+  # through literary research
+  #lambda_nav = 9.41 * 10^-6
+  
+  # Failure rate for the a drone's electronic system, this value was found
+  # through literary research
+  #lambda_es = 5.01 * 10^-6
+  
+  # Failure rate for the a drone's payload, this value was found
+  # through literary research
+  #lambda_pay = 1.10 * 10^-6
+  
+  # Failure rate for the a drone's power plant, this value was found
+  # through literary research, however we are using other values to calculate
+  # this
+  #lambda_pwr = 9.94 * 10^-6
+  
+  # Failure rate for the motor. The Mean Time To Failure was found through 
+  # researching different drones. The failure rate is the inverse of the 
+  # Mean Time To Failure
+  lambda_mot = 1/mot1_MTTF 
+  
+  # Failure rate for the new propeller. This is derived by taking the
+  # inverse of the Mean Time To Failure that was inputted to this function
+  lambda_mot2= 1/mot2_MTTF
+  
+  # Failure rate for the propeller. The Mean Time To Failure was
+  # found through researching different drones. The failure rate is the 
+  # inverse of the Mean Time To Failure
+  lambda_prop = 1/prop1_MTTF
+  
+  # Failure rate for the new propeller. This is derived by taking the
+  # inverse of the Mean Time To Failure that was inputted to this function
+  lambda_prop2 = 1/prop2_MTTF
+  
+  # Failure rate for a lithium ion battery. The Mean Time To Failure was
+  # derived by multiplying the average life of one cycle by the expected
+  # life cycles of the battery system. the failure rate is the inverse of
+  # the Mean Time To Failure
+  lambda_bat = 1/(bat1_t*bat1_lc)
+  
+  # Failure rate for the new battery system. This is derived by taking the
+  # inverse of the Mean Time To Failure that was inputted to this function
+  lambda_bat2 = 1/(bat2_t*bat2_lc)
+  
+  # Failure Rate for controls, this is derived by working backwards from the
+  # original power plant failure rate. Since we assume that components are
+  # independent, this stays constant even when we use the updated components
+  #ambda_cnt = lambda_pwr - (lambda_bat + (lambda_mot+lambda_prop)^2)
+  
+  # Creates a data frame for the reliability of each component at 6 intervals
+  myingredients = tibble(
+    t = seq(from = 0, to = 1000, by = 50),
+    prob_gc = 1-pexp(t, rate = lambda_gc),
+    prob_main = 1-pexp(t, rate = lambda_main),
+    prob_bat = pexp(t, rate = lambda_bat),
+    prob_mot = pexp(t, rate = lambda_mot),
+    prob_prop = pexp(t, rate = lambda_prop),
+    prob_nav = 1-pexp(t, rate = lambda_nav),
+    prob_es = 1-pexp(t, rate = lambda_es),
+    prob_pay = 1-pexp(t, rate = lambda_pay),
+    prob_mot2 = pexp(t, rate = lambda_mot2),
+    prob_prop2 = pexp(t, rate = lambda_prop2),
+    prob_bat2 = pexp(t, rate = lambda_bat2),
+    prob_pwr = 1-(prob_bat + (prob_mot + prob_prop)^2),
+    prob_pwr_newm = 1-(prob_bat + (prob_mot2 + prob_prop)^2),
+    prob_pwr_newb = 1-(prob_bat2 + (prob_mot + prob_prop)^2),
+    prob_pwr_newp = 1-(prob_bat + (prob_mot + prob_prop2)^2),
+    prob_pwr_newe = 1-(prob_bat2 + (prob_mot2 + prob_prop2)^2),
+    
+    # In order to fix any potential errors in our breakdown of the power system,
+    # we will 
+    prob_pwr_fixed = ifelse(prob_pwr < 0, 0, prob_pwr),
+    prob_pwrm_fixed = ifelse(prob_pwr_newm < 0, 0, prob_pwr_newm),
+    prob_pwrb_fixed = ifelse(prob_pwr_newb < 0, 0, prob_pwr_newb),
+    prob_pwrp_fixed = ifelse(prob_pwr_newp < 0, 0, prob_pwr_newp),
+    prob_pwre_fixed = ifelse(prob_pwr_newe < 0, 0, prob_pwr_newe)
+  )
+  
+  # Using the reliability calculated in myingredients, a binomial simulation
+  # is run 1000 times at each time interval. This allows us to collect more
+  # data using the expected probability and we can then find the average, 
+  # standard deviation, and standard error. With the standard error we are able
+  # to find confidence intervals which tell us whether there is a statistical
+  # significance in our results.
+  prob1 = myingredients %>%
+    group_by(t) %>% # organizes the data frame
+    reframe(
+      n = 1000, #number of simulations
+      reps = 1:n, 
+      gc = rbinom(n = n, size = 1, prob = prob_gc),
+      main = rbinom(n = n, size = 1, prob = prob_main),
+      pay = rbinom(n = n, size = 1, prob = prob_pay),
+      nav = rbinom(n = n, size = 1, prob = prob_nav),
+      es = rbinom(n = n, size = 1, prob = prob_es),
+      mot = rbinom(n = n, size = 1, prob = prob_mot),
+      prop = rbinom(n = n, size = 1, prob = prob_prop),
+      bat = rbinom(n = n, size = 1, prob = prob_bat),
+      pwr = rbinom(n = n, size = 1, prob = prob_pwr_fixed),
+      mot2 = rbinom(n = n, size = 1, prob = prob_mot2),
+      prop2 = rbinom(n = n, size = 1, prob = prob_prop2),
+      bat2 = rbinom(n = n, size = 1, prob = prob_bat2),
+      
+      pwr_newe = rbinom(n = n, size = 1, prob = prob_pwre_fixed),
+      pwr_newm = rbinom(n = n, size = 1, prob = prob_pwrm_fixed),
+      pwr_newb = rbinom(n = n, size = 1, prob = prob_pwrb_fixed),
+      pwr_newp = rbinom(n = n, size = 1, prob = prob_pwrp_fixed),
+      
+      # Calculates 
+      prob_T0 = gc*main*pwr*nav*es*pay,
+      prob_T1 = gc*main*pwr_newm*nav*es*pay,
+      prob_T2 = gc*main*pwr_newb*nav*es*pay,
+      prob_T3 = gc*main*pwr_newp*nav*es*pay,
+      prob_T4 = gc*main*pwr_newe*nav*es*pay
+    ) 
+  
+  
+  prob2 <- prob1 %>% #Main plot
+    group_by(t) %>%
+    summarize(
+      # Mean and Standard Deviation for the Reliability of each 
+      # system being tested
+      mean0 = mean(prob_T0),
+      sd0 = sd(prob_T0),
+      
+      mean1 = mean(prob_T1),
+      sd1 = sd(prob_T1),
+      
+      mean2 = mean(prob_T2),
+      sd2 = sd(prob_T2),
+      
+      mean3 = mean(prob_T3),
+      sd3 = sd(prob_T3),
+      
+      mean4 = mean(prob_T4),
+      sd4 = sd(prob_T4),
+      
+      # Number of simulations at each time period
+      n = n(),
+      
+      # Standard error for each system being analyzed
+      se0 = sd0/sqrt(n),
+      se1 = sd1/sqrt(n),
+      se2 = sd2/sqrt(n),
+      se3 = sd3/sqrt(n),
+      se4 = sd4/sqrt(n),
+      
+      # Approximated lower and upper 95% confidence intervals for each system
+      lower_approx0 = mean0 + qnorm(0.025) * se0,
+      upper_approx0 = mean0 + qnorm(0.975) * se0,
+      
+      lower_approx1 = mean1 + qnorm(0.025) * se1,
+      upper_approx1 = mean1 + qnorm(0.975) * se1,
+      
+      lower_approx2 = mean2 + qnorm(0.025) * se2,
+      upper_approx2 = mean2 + qnorm(0.975) * se2,
+      
+      lower_approx3 = mean3 + qnorm(0.025) * se3,
+      upper_approx3 = mean3 + qnorm(0.975) * se3,
+      
+      lower_approx4 = mean4 + qnorm(0.025) * se4,
+      upper_approx4 = mean4 + qnorm(0.975) * se4)
+  
+  
+  # Plots the reliability for each system. The meean is plotted as a black line
+  # and the 95% confidence interval is plotted as a ribbon
+  plot1 = ggplot() +
+    geom_line(data = prob2, mapping = aes(x = t, y = mean0)) +
+    geom_line(data = prob2, mapping = aes(x = t, y = mean1)) +
+    geom_line(data = prob2, mapping = aes(x = t, y = mean2)) +
+    geom_line(data = prob2, mapping = aes(x = t, y = mean3)) +
+    geom_line(data = prob2, mapping = aes(x = t, y = mean4)) +
+    
+    geom_ribbon(data = prob2, mapping = aes(x = t, ymin = lower_approx0, ymax = upper_approx0, fill = "Original"), alpha = 0.8) +
+    geom_ribbon(data = prob2, mapping = aes(x = t, ymin = lower_approx1, ymax = upper_approx1, fill = "New Motor"), alpha = 0.8) +
+    geom_ribbon(data = prob2, mapping = aes(x = t, ymin = lower_approx2, ymax = upper_approx2, fill = "New Battery"), alpha = 0.8) +
+    geom_ribbon(data = prob2, mapping = aes(x = t, ymin = lower_approx3, ymax = upper_approx3, fill = "New Propeller"), alpha = 0.8) +
+    geom_ribbon(data = prob2, mapping = aes(x = t, ymin = lower_approx4, ymax = upper_approx4, fill = "New Everything"), alpha = 0.8) +
+    
+    theme_classic(base_size = 14) +
+    labs(x = "Time (Hours)", y = "Reliability (%)",
+         title = "System Reliability of Commercial Drones for 1 year",
+         fill = "Scenario") + 
+    theme_minimal()
+  plot1
+  # Finds the reliability for each improved system and converts it into a percentage
+  new_m = prob2[6,4] * 100
+  new_p = prob2[6,8] * 100
+  new_b = prob2[6,6] * 100
+  new_e = prob2[6,10]* 100
+  
+  # Outputs a the reliability at one year for each improved system
+  #print(paste0("The expected reliability at 1 year for a drone with a new motor is ", new_m, "%"))
+  #print(paste0("The expected reliability at 1 year for a drone with a new propeller is ", new_p, "%"))
+  #print(paste0("The expected reliability at 1 year for a drone with a new battery is ", new_b, "%"))
+  #print(paste0("The expected reliability at 1 year for a drone with a new motor, propeller, and battery is ", new_e, "%"))
+  #return(plot1)
+  
+  tab = tibble(
+    id = 1:4,
+    statement = c(
+      paste0("The expected reliability at 1 year for a drone with a new motor is ", new_m, "%, with an expected range of "),
+      paste0("The expected reliability at 1 year for a drone with a new propeller is ", new_p, "%"),
+      paste0("The expected reliability at 1 year for a drone with a new battery is ", new_b, "%"),
+      paste0("The expected reliability at 1 year for a drone with a new motor, propeller, and battery is ", new_e, "%")
+    )
+  )  
+  
+  output = list(plot = plot1,
+                tab = tab)
+  
+  return(output)
+  
+}
+
+# d = drone_reliability(mot1_MTTF = 8000, mot2_MTTF = 12000, prop1_MTTF = 300, 
+#                       prop2_MTTF=500, bat1_t = 800, bat1_lc = 400,
+#                       bat2_t=1090, bat2_lc=1000,
+#                       lambda_gc = 2.00*10^-6, lambda_main = 2.77*10^-6,
+#                       lambda_nav = 9.41*10^-6, lambda_es = 5.01*10^-6,
+#                       lambda_pay = 1.10*10^-6)
+# d$plot
+# d$tab
--- z/dev.R
+++ z/dev.R
@@ -3,37 +3,40 @@
 #' @author Tim Fraser, your names here...
 #' @description Script for test package building of `demotool` package functions.
 
+
 # Unload your package and uninstall it first.
-unloadNamespace("demotool"); remove.packages("demotool")
+unloadNamespace("dronereliability"); remove.packages("dronereliability")
 # Auto-document your package, turning roxygen comments into manuals in the `/man` folder
 devtools::document(".")
 # Load your package temporarily!
 devtools::load_all(".")
 
+
 # Test out our functions
+dronereliability::get_stats()
 
 # Add 1 to x
-demotool::plus_one(x = 1)
-# Add 1 to a vector of x values
-demotool::plus_one(x = c(1,2,3,4))
-
-# Get series system probability at each time t
-demotool::get_prob(t = c(2,4,5,6), lambdas = c(0.001, 0.02), type = "series")
- 
-# Get parallel system probability at each time t
-demotool::get_prob(t = c(2,4,5,6), lambdas = c(0.001, 0.02), type = "parallel")
+# dronereliability::plus_one(x = 1)
+# # Add 1 to a vector of x values
+# dronereliability::plus_one(x = c(1,2,3,4))
+# 
+# # Get series system probability at each time t
+# dronereliability::get_prob(t = c(2,4,5,6), lambdas = c(0.001, 0.02), type = "series")
+#  
+# # Get parallel system probability at each time t
+# dronereliability::get_prob(t = c(2,4,5,6), lambdas = c(0.001, 0.02), type = "parallel")
 
 # Just think: you could make many functions,
 # outputting vectors, data.frames, ggplot visuals, etc.
 # So many options!
 
 # When finished, remember to unload the package
-unloadNamespace("demotool"); remove.packages("demotool")
+unloadNamespace("dronereliability"); remove.packages("dronereliability")
 
 # Then, when ready, document, unload, build, and install the package!
 # For speedy build, use binary = FALSE and vignettes = FALSE
 devtools::document(".");
-unloadNamespace("demotool");
+unloadNamespace("dronereliability");
 devtools::build(pkg = ".", path = getwd(), binary = FALSE, vignettes = FALSE)
 
 
@@ -41,14 +44,15 @@ devtools::build(pkg = ".", path = getwd(), binary = FALSE, vignettes = FALSE)
 # such as 
 # install.packages("nameofyourpackagefile.tar.gz", type = "source")
 # or in our case:
-install.packages("demotool_0.1.0.tar.gz", type = "source")
+install.packages("dronereliability_0.1.0.tar.gz", type = "source")
 
 # Load your package!
-library("demotool")
+library("dronereliability")
 
+get_stats()
 
 # When finished, remember to unload the package
-unloadNamespace("demotool"); remove.packages("demotool")
+unloadNamespace("dronereliability"); remove.packages("dronereliability")
 
 # Always a good idea to clear your environment and cache
 rm(list = ls()); gc()
